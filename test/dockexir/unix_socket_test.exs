26f2fe09e2af24e0296cf375299d9e4ae4c2af27
Jairo Luiz <t4ngz3r0@gmail.com>
2017-06-27 11:27:28 -0400

    more coverage

--- test/dockexir/unix_socket_test.exs
+++ test/dockexir/unix_socket_test.exs
@@ -1,25 +1,38 @@
+defmodule Dockexir.UnixSocketTest.Macros do
+  alias Dockexir.UnixSocket
+
+  defmacro test_delegate(function, args) do
+    quote do
+      with_mock :hackney_tcp, [:unstick], [{unquote(function), fn(unquote_splicing(args)) -> :ok end}] do
+        :ok = UnixSocket.unquote(function)(unquote_splicing(args))
+      end
+    end
+  end
+end
+
 defmodule Dockexir.UnixSocketTest do
-  use ExUnit.Case, async: false
+  use ExUnit.Case
   import Mock
+  import Dockexir.UnixSocketTest.Macros
   alias Dockexir.UnixSocket
 
   test "should connect to a Unix socket" do
     with_mock :gen_tcp, [:unstick], [connect: fn({:local, '/var/run/docker.sock'}, _, _, _) -> {:ok, :socket} end] do
       {:ok, conn} = :hackney.connect(UnixSocket, "/var/run/docker.sock", 0)
       assert is_reference(conn)
+      assert called :gen_tcp.connect(
+        {:local, '/var/run/docker.sock'}, 0,
+        [:binary, {:active, false}, {:packet, :raw}], 8000
+      )
     end
   end
 
-  test ".send should delegates to :hackney_tcp" do
-    with_mock :hackney_tcp, [:unstick], [send: fn(:socket, :data) -> :ok end] do
-      :ok = UnixSocket.send(:socket, :data)
-    end
-  end
-
-  test ".recv should delegates to :hackney_tcp" do
-    with_mock :hackney_tcp, [:unstick], [recv: fn(:socket, :length, :timeout) -> :ok end] do
-      :ok = UnixSocket.recv(:socket, :length, :timeout)
-    end
-  end
+  test_delegate :send, [:socket, :data]
+  test_delegate :recv, [:socket, :length, :timeout]
+  test_delegate :close, [:socket]
+  test_delegate :controlling_process, [:socket, :pid]
+  test_delegate :setopts, [:socket, :opts]
+  test_delegate :peername, [:socket]
+  test_delegate :messages, [:socket]
 
 end
